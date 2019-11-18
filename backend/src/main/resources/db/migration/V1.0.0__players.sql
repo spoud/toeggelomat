a3ea00108c094fdee23ea9a4ec8bc71189e11086
Gaetan Collaud <gaetan.collaud@spoud.io>
2019-11-18 18:11:30 +0100

    remove users

--- backend/src/main/resources/db/migration/V1.0.0__players.sql
+++ backend/src/main/resources/db/migration/V1.0.0__players.sql
@@ -4,13 +4,9 @@ CREATE TABLE t_player(
     nick_name VARCHAR(100) NOT NULL,
     email VARCHAR(200) NOT NULL,
     points INTEGER NOT NULL,
-    PRIMARY KEY (player_uuid)
+    PRIMARY KEY (player_uuid),
+    CONSTRAINT undx_player_nickname UNIQUE (nick_name),
+    CONSTRAINT undx_player_email UNIQUE (email)
 );
 
 
-INSERT INTO t_player (player_uuid, nick_name, email, points) VALUES('b480a9a6-00c5-11ea-8d71-362b9e155667', 'Anna', 'anna.messerli@spoud.io', 1000);
-INSERT INTO t_player (player_uuid, nick_name, email, points) VALUES('b480ac12-00c5-11ea-8d71-362b9e155667', 'Gaétan', 'gaetan.collaud@spoud.io', 1000);
-INSERT INTO t_player (player_uuid, nick_name, email, points) VALUES('b480ad5c-00c5-11ea-8d71-362b9e155667', 'Julian', 'julian.kampfli@spoud.io', 1000);
-INSERT INTO t_player (player_uuid, nick_name, email, points) VALUES('b480ae88-00c5-11ea-8d71-362b9e155667', 'Marcel', 'marcel.baur@spoud.io', 1000);
-INSERT INTO t_player (player_uuid, nick_name, email, points) VALUES('b480afb4-00c5-11ea-8d71-362b9e155667', 'Patrick', 'patrick.boenzli@spoud.io', 1000);
-INSERT INTO t_player (player_uuid, nick_name, email, points) VALUES('b480b2b6-00c5-11ea-8d71-362b9e155667', 'Lukas', 'lukas.zaugg@spoud.io', 1000);
