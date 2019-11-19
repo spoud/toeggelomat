86ddaaf2d5131f6b3ea056f00a8a0e8d1e4d3fac
Gaetan Collaud <gaetan.collaud@spoud.io>
2019-11-19 11:24:46 +0100

    merge sql files

--- backend/src/main/resources/db/migration/V1.0.0__players.sql
+++ backend/src/main/resources/db/migration/V1.0.0__players.sql
@@ -3,7 +3,8 @@ CREATE TABLE t_player(
     player_uuid UUID NOT NULL,
     nick_name VARCHAR(100) NOT NULL,
     email VARCHAR(200) NOT NULL,
-    points INTEGER NOT NULL,
+    defense_points INTEGER NOT NULL,
+    offense_points INTEGER NOT NULL,
     PRIMARY KEY (player_uuid),
     CONSTRAINT undx_player_nickname UNIQUE (nick_name),
     CONSTRAINT undx_player_email UNIQUE (email)
