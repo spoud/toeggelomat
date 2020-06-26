b1d511db806cbed30bc238027eb690b7cbd4c46c
Tartori <julian.stampfli@spoud.io>
2020-06-26 20:53:40 +0200

    player topic (#21)

--- backend/src/main/java/io/spoud/producer/PlayerBO.java
+++ backend/src/main/java/io/spoud/producer/PlayerBO.java
@@ -1,4 +1,20 @@
 package io.spoud.producer;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@RegisterForReflection
 public class PlayerBO {
+  private UUID uuid;
+  private String nickName;
+  private String email;
+  private String slackId;
 }
