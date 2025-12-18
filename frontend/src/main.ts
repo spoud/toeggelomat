531f8f92bd257c846823b3f5dd799c64c5d2456d
Gaétan Collaud <gaetancollaud@gmail.com>
2025-12-18 13:55:28 +0100

    update frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,4 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import {bootstrapApplication} from "@angular/platform-browser";
 import {AppComponent} from "./app/app.component";
 import {appConfig} from './app/app.config';
