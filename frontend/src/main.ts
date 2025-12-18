9bfe67c31c890d4819b75a03ef373cedacb88eaf
Gaétan Collaud <gaetancollaud@gmail.com>
2025-12-18 13:59:24 +0100

    remove zone.js

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,3 @@
-import { provideZoneChangeDetection } from "@angular/core";
 import {bootstrapApplication} from "@angular/platform-browser";
 import {AppComponent} from "./app/app.component";
 import {appConfig} from './app/app.config';
